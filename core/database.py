3f584ac857753b1fe890869dd063dde9f370fcd1
Kum-Borise-Tham--globaluser.email <kumarianetham@gmail.com>
2026-06-24 05:45:28 +0200

    removed load_gotenv to used render env variables

--- core/database.py
+++ core/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
+# Remove load_dotenv() — use Render environment variables directly
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
